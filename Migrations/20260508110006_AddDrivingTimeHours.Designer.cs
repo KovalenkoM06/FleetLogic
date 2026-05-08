2d188ffb06a4002fe177b95e07cd2de310a4e035
KovalenkoM06 <makskovalenko2099@gmail.com>
2026-05-08 14:00:44 +0300

    bd telemetry

--- Migrations/20260508110006_AddDrivingTimeHours.Designer.cs
+++ Migrations/20260508110006_AddDrivingTimeHours.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using FleetLogic.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace FleetLogic.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260508110006_AddDrivingTimeHours")]
+    partial class AddDrivingTimeHours
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Migrations/20260508110006_AddDrivingTimeHours.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FleetLogic.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddDrivingTimeHours : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "DrivingTimeHours",
+                table: "TelemetryLogs",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DrivingTimeHours",
+                table: "TelemetryLogs");
+        }
+    }
+}
